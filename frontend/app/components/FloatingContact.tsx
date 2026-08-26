8cbbf6efc08adda3f68d224d4005337873857dd2
duyhoang01 <giangnguyen9711@gmail.com>
2026-08-26 15:43:02 +0700

    fix: use real Facebook icon asset instead of hand-drawn SVG glyph

--- frontend/app/components/FloatingContact.tsx
+++ frontend/app/components/FloatingContact.tsx
@@ -1,20 +1,13 @@
 "use client";
 
-function FacebookIcon() {
-  return (
-    <svg viewBox="0 0 320 512" width="20" height="20" fill="white">
-      <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" />
-    </svg>
-  );
-}
-
 const buttons = [
   {
     label: "Facebook",
     href: "https://www.facebook.com/giangnc11",
     external: true,
-    bg: "#1877F2",
-    icon: <FacebookIcon />,
+    bg: "white",
+    // eslint-disable-next-line @next/next/no-img-element
+    icon: <img src="/icon/Facebook.png" alt="Facebook" className="w-11 h-11 object-contain" />,
   },
   {
     label: "Zalo",
